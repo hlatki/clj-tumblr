be0ea7ddf0c31c4b39207e48dde9eefbde224d6b
hlatki <hannahatkinson@gmail.com>
2013-03-03 23:22:06 +0400

    Added initial dependencies

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-tumblr "0.1.0-SNAPSHOT"
+  :description "Simple wrapper for Tumblr API(v2)"
+  :url "http://github.com/hlatki/clj-tumblr"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                [cheshire "5.0.2"]
+                [clj-http "0.6.4"]
+                ])
---
+++ test/clj_tumblr/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-tumblr.core-test
+  (:use clojure.test
+        clj-tumblr.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
